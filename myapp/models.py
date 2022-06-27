8b5a31c8ec00986d6518e064dabf84d4ea975bc2
lodyne <lodgmtui@gmail.com>
2022-06-27 09:53:58 +0300

    build(models):make models into database
    ~ make Post model with fields
    ~ register model into admin.py
    ~ migrate into the database

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Post(models.Model):
+    """Model definition for Post."""
+
+    # TODO: Define fields here
+    heading = models.CharField(max_length=50)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+
+    
+    class Meta:
+        """Meta definition for Post."""
+
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    def __str__(self):
+        """Unicode representation of Post."""
+        return self.heading
+
